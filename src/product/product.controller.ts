76930e7cc5dd6d8de6dfc685c4c46f96fe449145
Kristoffer Nielsen <kristoffer2509@icloud.com>
2019-04-09 16:18:11 +0200

    Experimenting with swagger auto generated docs

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -26,12 +26,61 @@ class ProductController implements Controller {
     this.router.delete(`${this.path}/:id`, authMiddleware, this.deleteProduct)
   }
 
+  /**
+ * @swagger
+ * definition:
+ *   product:
+ *     properties:
+ *       name:
+ *         type: string
+ *       description:
+ *         type: string
+ *       price:
+ *         type: number
+ */
+
+  /**
+   * @swagger
+   * /product:
+   *    get:
+   *      tags:
+   *        - "Products"
+   *      description: Returns all products
+   *      produces:
+   *        - application/json
+   *      responses:
+   *        200:
+   *          description: An array of products
+   *          schema:
+   *            $ref: '#/definitions/product'
+   */
   getAllProducts = async (req: express.Request, res: express.Response) => {
     const products = await this.product.find()
       .populate('owner', '-password').populate('category')
     res.send(products)
   }
 
+  /**
+   * @swagger
+   * /product/:id:
+   *    get:
+   *      tags:
+   *        - "Products"
+   *      description: Returns the product with the specified id
+   *      produces:
+   *       - application/json
+   *      parameters:
+   *       - name: id
+   *         description: The id of the product
+   *         required: true
+   *      responses:
+   *        200:
+   *         description: A single product
+   *         schema:
+   *          $ref: '#definitions/product'
+   *        404:
+   *         description: Product with the specified id was not found
+   */
   getProduct = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const id = req.params.id
     const product = await this.product.findById(id)
@@ -43,6 +92,31 @@ class ProductController implements Controller {
     }
   }
 
+  /**
+   * @swagger
+   * /product:
+   *    post:
+   *      tags:
+   *        - "Products"
+   *      description: Creates a new product
+   *      produces:
+   *       - application/json
+   *      parameters:
+   *       - name: Name
+   *         description: The products name
+   *         required: true
+   *       - name: Description
+   *         description: Describe the product
+   *         required: true
+   *       - name: Price
+   *         description: The products price
+   *         required: true
+   *      responses:
+   *        200:
+   *         description: Succesfully created
+   *         schema:
+   *          $ref: '#definitions/product'
+   */
   createProduct = async (req: RequestWithUser, res: express.Response) => {
     const productData: Product = req.body;
     const createdProduct = new this.product({
@@ -54,6 +128,25 @@ class ProductController implements Controller {
     res.send(savedProduct)
   }
 
+  /**
+   * @swagger
+   * /product/:id:
+   *    delete:
+   *      tags:
+   *        - "Products"
+   *      description: Deletes the product with the specified id
+   *      produces:
+   *       - application/json
+   *      parameters:
+   *       - name: id
+   *         description: The id of the product
+   *         required: true
+   *      responses:
+   *        200:
+   *         description: Succesfully deleted
+   *        404:
+   *         description: Product with the specified id was not found
+   */
   deleteProduct = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const id = req.params.id
     const successResponse = await this.product.findByIdAndDelete(id)
